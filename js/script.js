36952a4325b1738bb1c7f343c8b9e37977b82115
MalkitKaur <malkit27407@gmail.com>
2023-11-26 14:07:00 -0500

    added conditional statement

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ class Pizza {
     }
 
     getDescription() {
+        if (!this.crust) {
+            return "Error: Please select a crust for your pizza.";
+        }
+
         const toppingList = this.toppings.length > 0 ? `with ${this.toppings.join(', ')} toppings` : 'with no toppings';
         return `Thank you for ordering with us! You ordered a ${this.size}-sized pizza with ${this.crust} crust ${toppingList} and ${this.dippingSauce} dipping sauce.`;
     }
@@ -14,15 +18,18 @@ class Pizza {
 
 function placeOrder() {
     const pizzaSize = document.getElementById('pizza-size').value;
-    const crust = document.querySelector('input[name="crust"]:checked').value;
+    const crust = document.querySelector('input[name="crust"]:checked');
     const toppings = document.querySelectorAll('input[name="topping"]:checked');
     const dippingSauce = document.getElementById('dipping-sauce').value;
 
-    const pizzaOrder = new Pizza(pizzaSize, crust, toppings, dippingSauce);
+    // Check if crust is selected
+    const crustValue = crust ? crust.value : null;
+
+    const pizzaOrder = new Pizza(pizzaSize, crustValue, toppings, dippingSauce);
 
     const pizzaDescription = document.getElementById('pizza-description');
     pizzaDescription.innerHTML = `<p>Order Summary: ${pizzaOrder.getDescription()}</p>`;
 
     const studentInfo = document.getElementById('student-info');
-    studentInfo.textContent = `Student ID: 200543614`;
+    studentInfo.textContent = `Student ID: 200543614 | Name: Malkit Kaur`;
 }
